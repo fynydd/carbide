7a1f85bc8b5a6e597da772cf6015f55f9a70d10d
Michael Argentini <michael@argentini.us>
2018-04-09 19:50:20 -0400

    Added HttpContextApplication extension methods

--- Carbide/ExtensionMethods.cs
+++ Carbide/ExtensionMethods.cs
@@ -882,4 +882,51 @@ namespace Argentini.Carbide
 
         #endregion
     }
+
+    #region Context
+
+    /// <summary>
+    /// <![CDATA[
+    /// The HttpContextApplicationExtensions class contains methods and properties for enhancing 
+    /// HttpContext objects. To begin using, just include this assembly.
+    /// ]]>
+    /// </summary>
+    public static class HttpContextApplicationExtensions
+    {
+        /// <summary>
+        /// Determine if an Application-level state variable exists.
+        /// </summary>
+        /// <param name="application">The current HttpApplictionState object</param>
+        /// <param name="keyName">The key name of the Application-level state variable</param>
+        public static bool KeyExists(this HttpApplicationState application, string keyName)
+        {
+            bool result = false;
+
+            if (application != null)
+            {
+                if (application[keyName] != null)
+                {
+                    result = true;
+                }
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Remove an Application-level state variable, first checking for its existence
+        /// and a valid context.
+        /// </summary>
+        /// <param name="application">The current HttpApplictionState object</param>
+        /// <param name="keyName">The key name of the Application-level state variable</param>
+        public static void SafeRemove(this HttpApplicationState application, string keyName)
+        {
+            if (KeyExists(application, keyName))
+            {
+                application.Remove(keyName);
+            }
+        }
+    }
+
+    #endregion
 }
