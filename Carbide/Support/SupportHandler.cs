05e0916f94abaedd0f57308d8818d2c2c5bbd17e
Michael Argentini <michael@argentini.us>
2018-05-02 12:21:02 -0400

    Re-ordered cache rebuild activities, tweaked output

--- Carbide/Support/SupportHandler.cs
+++ Carbide/Support/SupportHandler.cs
@@ -102,7 +102,7 @@ namespace Argentini.Carbide
 
 						context.Application["RebuildCacheHistory"] += "<ol style=\"padding: 0.25rem 0 0 1rem;\">";
 
-                        context.Application["RebuildCacheHistory"] += "<li style=\"padding-bottom: 1rem;\">Clearing ImageProcessor cache... ";
+                        context.Application["RebuildCacheHistory"] += "<li style=\"padding-bottom: 1rem;\">Clearing cached images... ";
                         timer2.Start();
 
                         foreach (var folder in StorageHelpers.GetFolders("/App_Data/cache/"))
@@ -148,33 +148,36 @@ namespace Argentini.Carbide
                         timer2.Stop();
                         context.Application["RebuildCacheHistory"] += "<strong>completed in " + timer2.GetTime() + " seconds</strong></li>";
 
-                        context.Application["RebuildCacheHistory"] += "<li style=\"padding-bottom: 1rem;\">Republishing content... ";
+                        context.Application["RebuildCacheHistory"] += "<li style=\"padding-bottom: 1rem;\">Republishing all content... ";
                         timer2.Reset();
                         timer2.Start();
                         Services.ContentService.RePublishAll();
                         timer2.Stop();
                         context.Application["RebuildCacheHistory"] += "<strong>completed in " + timer2.GetTime() + " seconds</strong></li>";
 
-						context.Application["RebuildCacheHistory"] += "<li style=\"padding-bottom: 1rem;\">Rebuilding Examine indexes... ";
-                        timer2.Reset();
-                        timer2.Start();
-
-                        foreach (var index in ExamineManager.Instance.IndexProviderCollection.ToList())
-                        {
-							context.Application["RebuildCacheHistory"] += index.Name.Replace("Indexer", "") + "... ";
-                            index.RebuildIndex();
-                        }
-
-                        timer2.Stop();
-                        context.Application["RebuildCacheHistory"] += "<strong>completed in " + timer2.GetTime() + " seconds</strong></li>";
-
-						context.Application["RebuildCacheHistory"] += "<li style=\"padding-bottom: 1rem;\">Refreshing content... ";
+						context.Application["RebuildCacheHistory"] += "<li style=\"padding-bottom: 1rem;\">Refreshing XML cache... ";
                         timer2.Reset();
                         timer2.Start();
                         umbraco.library.RefreshContent();
                         timer2.Stop();
                         context.Application["RebuildCacheHistory"] += "<strong>completed in " + timer2.GetTime() + " seconds</strong></li>";
 
+                        context.Application["RebuildCacheHistory"] += "<li style=\"padding-bottom: 1rem;\">Rebuilding Examine indexes... ";
+                        timer2.Reset();
+                        timer2.Start();
+
+                        foreach (var index in ExamineManager.Instance.IndexProviderCollection.ToList())
+                        {
+                            context.Application["RebuildCacheHistory"] += index.Name.Replace("Indexer", "") + "... ";
+                            index.RebuildIndex();
+                        }
+
+                        timer2.Stop();
+                        context.Application["RebuildCacheHistory"] += "<strong>completed in " + timer2.GetTime() + " seconds</strong></li>";
+
+
+                        
+                        
                         timer.Stop();
 
                         context.Application.SafeRemove("RebuildCacheStatus");
