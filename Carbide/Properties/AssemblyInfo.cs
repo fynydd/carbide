b666a842b4564691cd110c15b71a29cf81d4128a
Michael Argentini <michael@argentini.us>
2018-05-14 17:27:00 -0400

    Added initial nuget build support

--- Carbide/Properties/AssemblyInfo.cs
+++ Carbide/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Carbide")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Carbide Tools for Umbraco")]
+[assembly: AssemblyDescription("A treasure trove of useful extension methods, helpers, and tools for Umbraco CMS.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Michael Argentini")]
 [assembly: AssemblyProduct("Carbide")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("Copyright © 2018, Michael Argentini")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
---
+++ Carbide/build-nuget.bat
@@ -0,0 +1,1 @@
+nuget pack Carbide.csproj -properties Configuration=Release -IncludeReferencedProjects
