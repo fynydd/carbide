b19d2cd477c5d57d773442f5cce4cc0667059a4d
Michael Argentini <michael@argentini.us>
2018-05-14 19:16:29 -0400

    Nuget config transforms working, dashcontrol is now embedded

--- Carbide/Support/SupportHandler.cs
+++ Carbide/Support/SupportHandler.cs
@@ -21,6 +21,34 @@ namespace Argentini.Carbide
     public class CarbideSupportController : UmbracoApiController
     {
         [HttpGet]
+        public HttpResponseMessage Html(string file) // /umbraco/api/carbidesupport/html/?file=DashControl
+        {
+            string result = "";
+
+            try
+            {
+                var assembly = Assembly.GetExecutingAssembly();
+                var fileName = "Carbide.Support.Html." + file + ".html";
+
+                using (Stream stream = assembly.GetManifestResourceStream(fileName))
+                {
+                    using (StreamReader reader = new StreamReader(stream))
+                    {
+                        result = reader.ReadToEnd();
+                    }
+                }
+            }
+
+            catch
+            {
+            }
+
+            var response = new HttpResponseMessage(HttpStatusCode.OK);
+            response.Content = new StringContent(result, Encoding.UTF8, "text/html");
+            return response;
+        }
+
+        [HttpGet]
         public HttpResponseMessage Scripts(string file) // /umbraco/api/carbidesupport/scripts/?file=FormValidationHelpers
         {
             string result = "";
