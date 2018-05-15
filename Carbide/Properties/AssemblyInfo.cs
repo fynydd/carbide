36e95f5f7b99f5ebf4c7cf4170ae9da0ec8c0d26
Michael Argentini <michael@argentini.us>
2018-05-14 20:47:42 -0400

    Reverted version number

--- Carbide/Properties/AssemblyInfo.cs
+++ Carbide/Properties/AssemblyInfo.cs
@@ -32,9 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.1")]
-[assembly: AssemblyFileVersion("1.0.1")]
-
-// Embedded JavaScript resources
-// USAGE: <script src="@Html.Raw(Url.Content("~/umbraco/api/halidesupport/scripts/?file=FormValidationHelpers"))"></script>
-[assembly: System.Web.UI.WebResource("FormValidationHelpers.js", "application/x-javascript")]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
