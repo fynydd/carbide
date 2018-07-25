ffe58f3fe23f130dd627ff1f1ad663f3b9b2f84d
Michael Argentini <michael@argentini.us>
2018-07-25 17:48:13 -0400

    DashControl now added from embedded resource

--- Carbide/Support/EventHandlers.cs
+++ Carbide/Support/EventHandlers.cs
@@ -40,37 +40,15 @@ namespace Fynydd.Carbide
 
             if (result == false)
             { 
-                //<section alias="CarbideContentTools">
-                //  <areas>
-                //    <area>content</area>
-                //  </areas>
-                //  <tab caption="Carbide Content Tools">
-                //    <control>/umbraco/api/carbidesupport/html/?file=DashControl</control>
-                //  </tab>
-                //</section>
-
                 XmlDocument xmlDoc = new XmlDocument();
                 xmlDoc.Load(Storage.MapPath(file));
                 XmlNode root = xmlDoc.DocumentElement;
 
-                XmlElement section = xmlDoc.CreateElement("section");
-                section.SetAttribute("alias", "CarbideContentTools");
+                XmlDocument fragment = new XmlDocument();
+                fragment.LoadXml(Storage.CarbideEmbeddedHtml("DashControl.xml"));
+                XmlNode tab = xmlDoc.ImportNode(fragment.DocumentElement, true);
 
-                XmlElement areas = xmlDoc.CreateElement("areas");
-                XmlElement area = xmlDoc.CreateElement("area");
-                area.InnerText = "content";
-                areas.AppendChild(area);
-
-                XmlElement tab = xmlDoc.CreateElement("tab");
-                tab.SetAttribute("caption", "Carbide Content Tools");
-                XmlElement control = xmlDoc.CreateElement("control");
-                control.InnerText = "/umbraco/api/carbidesupport/html/?file=DashControl";
-                tab.AppendChild(control);
-
-                section.AppendChild(areas);
-                section.AppendChild(tab);
-
-                root.InsertBefore(section, root.FirstChild);
+                root.InsertBefore(tab, root.FirstChild);
 
                 xmlDoc.Save(Storage.MapPath(file));
             }
