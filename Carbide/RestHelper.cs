b55da415e3df217b2f57aa571ddd75213aa742ac
Michael Argentini <michael@argentini.us>
2018-12-01 21:57:50 -0500

    Added RestSecurity class and methods for rate limiting anonymous REST calls

--- Carbide/RestHelper.cs
+++ Carbide/RestHelper.cs
@@ -3,26 +3,219 @@ using System.Collections.Generic;
 using System.Net;
 using System.IO;
 using System.Text;
+using System.Web;
 
 using Fynydd.Carbide.Constants;
 
 namespace Fynydd.Carbide
 {
-    /// <summary><![CDATA[
-    /// Simple class for performing HTTP REST requests.
-    /// ]]></summary>
-    /// <example>
-    /// <code><![CDATA[
-    /// RestHelper request = new RestHelper();
-    /// request.Url="http://seeker.dice.com/jobsearch/servlet/JobSearch";
-    /// request.HeaderItems.Add("X-App-Token","skadjfhaskjfs");
-    /// request.RequestType = RestHelper.RequestTypeEnum.GET;
-    /// request.RequestFormat = RestHelper.RequestFormatEnum.JSON;
-    /// request.Timeout = 10000;
-    /// string result = request.Call();
-    /// ]]></code>
-    /// </example>
-    public class RestHelper
+	/// <summary>
+	/// Various configuration settings for REST security calls.
+	/// </summary>
+	public class RestSecurityConfig
+	{
+		/// <summary>
+		/// Minimum number of seconds allowed between successive REST calls, per WAN address.
+		/// Default: 2
+		/// </summary>
+		public int RestApiRateLimitSeconds = 2;
+
+		/// <summary>
+		/// Maximum failure count within RestApiBlacklistMaxFailureSeconds before a caller is blacklisted.
+		/// Default: 25
+		/// </summary>
+		public int RestApiBlacklistMaxFailureCount = 25;
+
+		/// <summary>
+		/// Timeframe used to blacklist a caller for successive bad requests.
+		/// Default: 300
+		/// </summary>
+		public int RestApiBlacklistMaxFailureSeconds = 60 * 5;
+
+		/// <summary>
+		/// Number of seconds a blacklisted WAN IP address is blacklisted.
+		/// Default: 3,600
+		/// </summary>
+		public int RestApiBlacklistSeconds = 60 * 60;
+
+		/// <summary>
+		/// Age of a caller-provided timestamp in minutes considered valid.
+		/// Default: 15
+		/// </summary>
+		public int RestApiTimestampMinutes = 15;
+	}
+
+
+	/// <summary>
+	/// Class for returning REST results
+	/// </summary>
+	public class RestSecurityResult
+	{
+		/// <summary>
+		/// WAN IP address
+		/// </summary>
+		public string WanIp { get; set; }
+
+		/// <summary>
+		/// HTTP status code
+		/// </summary>
+		public HttpStatusCode StatusCode { get; set; }
+
+		/// <summary>
+		/// HTTP status code text (e.g. OK)
+		/// </summary>
+		public string Result { get; set; }
+
+		/// <summary>
+		/// Verbose result description
+		/// </summary>
+		public string Message { get; set; }
+	}
+
+	/// <summary><![CDATA[
+	/// Various helper methods for ensuring REST call security.
+	/// ]]></summary>
+	public static class RestSecurity
+	{
+		/// <summary>
+		/// Get the RestSecurity cache name
+		/// </summary>
+		/// <param name="wanIp">WAN IP address</param>
+		/// <returns></returns>
+		public static string GetWanIpCacheName(string wanIp)
+		{
+			return "IP" + wanIp.Replace(".", "-");
+		}
+
+		/// <summary>
+		/// Simple way to rate limit and blacklist REST requests from anonymous website pages (no tokens).
+		/// </summary>
+		/// <param name="wanIpAddress">The WAN IP address of the caller (provided by the caller)</param>
+		/// <param name="timestamp">UTC timestamp of the REST call (provided by the caller) in the format 2018-12-01T23:59:00Z, or empty string to not use timestamps</param>
+		/// <param name="config">RestSecurityConfig object</param>
+		/// <param name="context">Optional HttpContext object (uses Current if not provided)</param>
+		/// <returns>RestSecurityResult object with response information</returns>
+		public static RestSecurityResult RateLimit(string wanIpAddress, string timestamp, RestSecurityConfig config, HttpContext context = null)
+		{
+			var wanip = ContextHelpers.EnsureAppContext(context).Request.UserHostAddress;
+			var result = new RestSecurityResult { WanIp = wanip, StatusCode = HttpStatusCode.BadGateway, Result = "Bad Gateway", Message = "Invalid request" };
+
+			try
+			{
+				if (CacheHelpers.CacheExists("BLACKLISTED" + GetWanIpCacheName(wanip)))
+				{
+					result.StatusCode = HttpStatusCode.Forbidden;
+					result.Result = "Blacklisted";
+					result.Message = "You appear to be up to no good.";
+				}
+
+				else
+				{
+					// If a cache item exists with the WAN IP, rate limit (deny request)
+					if (CacheHelpers.CacheExists(GetWanIpCacheName(wanip)))
+					{
+						result.StatusCode = (HttpStatusCode)429;
+						result.Result = "Too Many Requests";
+						result.Message = "Slow down there partner.";
+					}
+
+					else
+					{
+						CacheHelpers.CacheAdd(GetWanIpCacheName(wanip), 0, expirationSeconds: config.RestApiRateLimitSeconds);
+
+						// Validate WAN IP address matches actual
+						if (string.IsNullOrWhiteSpace(wanIpAddress) == false && wanip == wanIpAddress)
+						{
+							if (string.IsNullOrEmpty(timestamp))
+							{
+								// Timestamp not used, request is good...
+
+								result.StatusCode = HttpStatusCode.OK;
+								result.Result = "OK";
+								result.Message = "";
+							}
+
+							else
+							{
+								// Enforce UTC timestamp format of 2018-12-01T23:59:00Z
+								if (timestamp.Length > 19 && timestamp.Contains("T") && timestamp.EndsWith("Z"))
+								{
+									if (string.IsNullOrWhiteSpace(timestamp) == false && timestamp.IsDate() && DateTime.Parse(timestamp, null, System.Globalization.DateTimeStyles.AdjustToUniversal).DateDiff<int>(DateTime.Now.ToUniversalTime(), DateDiffComparisonType.Minutes) <= config.RestApiTimestampMinutes)
+									{
+										// Validate timestamp, request is good...
+
+										result.StatusCode = HttpStatusCode.OK;
+										result.Result = "OK";
+										result.Message = "";
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+
+			catch (Exception e)
+			{
+				result.StatusCode = HttpStatusCode.InternalServerError;
+				result.Result = "Internal Server Error";
+				result.Message = e.Message;
+			}
+
+			ProcessRestSecurityTracking(result, config);
+
+			return result;
+		}
+
+		/// <summary>
+		/// Process and track REST request tracking.
+		/// </summary>
+		/// <param name="result">REST request result to process and track</param>
+		/// <param name="config">REST security configuration</param>
+		public static void ProcessRestSecurityTracking(RestSecurityResult result, RestSecurityConfig config)
+		{
+			// Failure, track bad requests for potential blacklisting
+			if (result.StatusCode != HttpStatusCode.OK)
+			{
+				int failures = 0;
+				var _x = HttpContext.Current.Cache;
+
+				if (CacheHelpers.CacheExists("FAILURES" + GetWanIpCacheName(result.WanIp)))
+				{
+					failures = CacheHelpers.Cache<int>("FAILURES" + GetWanIpCacheName(result.WanIp));
+				}
+
+				failures++;
+
+				if (failures > config.RestApiBlacklistMaxFailureCount)
+				{
+					CacheHelpers.CacheDelete("FAILURES" + GetWanIpCacheName(result.WanIp));
+					CacheHelpers.CacheAdd("BLACKLISTED" + GetWanIpCacheName(result.WanIp), 0, expirationSeconds: config.RestApiBlacklistSeconds);
+				}
+
+				else
+				{
+					CacheHelpers.CacheAdd("FAILURES" + GetWanIpCacheName(result.WanIp), failures, expirationSeconds: config.RestApiBlacklistMaxFailureSeconds);
+				}
+			}
+		}
+	}
+
+	/// <summary><![CDATA[
+	/// A class for performing HTTP REST requests.
+	/// ]]></summary>
+	/// <example>
+	/// <code><![CDATA[
+	/// RestHelper request = new RestHelper();
+	/// request.Url="http://seeker.dice.com/jobsearch/servlet/JobSearch";
+	/// request.HeaderItems.Add("X-App-Token","skadjfhaskjfs");
+	/// request.RequestType = RestHelper.RequestTypeEnum.GET;
+	/// request.RequestFormat = RestHelper.RequestFormatEnum.JSON;
+	/// request.Timeout = 10000;
+	/// string result = request.Call();
+	/// ]]></code>
+	/// </example>
+	public class RestHelper
     {
         #region Properties and variables
 
