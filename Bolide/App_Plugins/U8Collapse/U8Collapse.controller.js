45a6a665aabeaff77d0026400b20cbe587857f0f
Michael Argentini <michael@argentini.us>
2019-06-26 11:10:02 -0400

    Added U8 Collapse package and modified its controller

---
+++ Bolide/App_Plugins/U8Collapse/U8Collapse.controller.js
@@ -0,0 +1,30 @@
+﻿angular.module('umbraco.services').config([
+    '$httpProvider',
+    function ($httpProvider) {
+        $httpProvider.interceptors.push(function ($q, $location) {
+            return {
+                'response': function (response) {
+                    if (response.config.url.includes('/umbraco/backoffice/UmbracoApi/Content/GetById')
+                        || response.config.url.includes('umbraco/backoffice/UmbracoApi/Content/GetEmpty')) {
+                        if (response.status === 200) {
+                            window.setTimeout(function () {
+                                var tabs = document.getElementsByClassName("umb-group-panel__header");
+                                for (var i = 0; i < tabs.length; i++) {
+                                    if (tabs[i].onclick == null) {
+                                        tabs[i].onclick = function () {
+                                            this.classList.toggle("open");
+                                            this.nextElementSibling.classList.toggle("open");
+                                        }
+                                    }
+                                }
+                                tabs[0].classList.add("open");
+                                tabs[0].nextElementSibling.classList.add("open");
+                            }, 10)
+                        }
+                    }
+                    return response;
+                }
+            };
+        });
+    }
+]);
