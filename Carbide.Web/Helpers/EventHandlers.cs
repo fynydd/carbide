73b2935cf13db16dd1ca2989268a99c84e1d330b
Michael Argentini <michael@argentini.us>
2019-07-24 17:05:38 -0400

    Umbraco form names modified with user id, form access for users changed

--- Carbide.Web/Helpers/EventHandlers.cs
+++ Carbide.Web/Helpers/EventHandlers.cs
@@ -1,15 +1,20 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 
 using Umbraco.Core;
 using Umbraco.Core.Composing;
 using Umbraco.Core.Events;
+using Umbraco.Core.Models.Membership;
 using Umbraco.Core.Services;
 using Umbraco.Core.Services.Implement;
 
 using Umbraco.Forms.Core;
-using Umbraco.Forms.Core.Data.Storage;
+using Umbraco.Forms.Core.Models;
+using Umbraco.Forms.Core.Persistence.Dtos;
 using Umbraco.Forms.Data.Storage;
+using Umbraco.Forms.Core.Data.Storage;
 
 using Umbraco.Web;
 
@@ -25,83 +30,107 @@ namespace MyProject.Components
     {
         public void Compose(Composition composition)
         {
-            //composition.SetContentLastChanceFinder<My404ContentFinder>();
-
             composition.Components().Append<MyComponent>();
         }
     }
 
     public class MyComponent : IComponent
     {
-        private readonly IContentService _contentService;
         private readonly IUmbracoContextFactory _context;
         private readonly IFormStorage _formStorage;
+        private readonly IUserSecurityStorage _userSecurityStorage;
+        private readonly IUserFormSecurityStorage _userFormSecurityStorage;
+        private readonly IUserService _userService;
+        //private readonly IMembershipUserService _membershipUserService;
 
-        public MyComponent(IContentService contentService, IUmbracoContextFactory context, IFormStorage formStorage)
+        public MyComponent(IFormStorage formStorage, IUserService userService, IUserSecurityStorage userSecurityStorage, IUserFormSecurityStorage userFormSecurityStorage, /*IMembershipUserService membershipUserService,*/ IUmbracoContextFactory context)
         {
-            _contentService = contentService;
-            _context = context;
             _formStorage = formStorage;
+            _userService = userService;
+            _userSecurityStorage = userSecurityStorage;
+            _userFormSecurityStorage = userFormSecurityStorage;
+            //_membershipUserService = membershipUserService;
+            _context = context;
         }
 
         public void Initialize()
         {
-            ContentService.Saving += this.ContentService_Saving;
-            //FormStorage.Created += this.FormStorage_Created;
+            _formStorage.Saving += this.FormStorage_Saving;
+            _formStorage.Saved += this.FormStorage_Saved;
         }
 
         public void Terminate()
         {
         }
 
-        /// <summary>
-        /// Listen for when content is being saved, check if it is a
-        /// new item and fill in some default data.
-        /// </summary>
-        private void ContentService_Saving(IContentService sender, ContentSavingEventArgs e)
+        //private void MembershipUserService_Save(object sender, SaveEventArgs<IUser> e)
+        //{
+
+        //}
+
+        private void FormStorage_Saving(object sender, FormEventArgs e)
         {
-            //foreach (var content in e.SavedEntities.Where(c => c.ContentType.Alias.InvariantEquals("quotation")))
-            //{
-            //    if (content.HasProperty("quote") && content.HasProperty("author"))
-            //    {
-            //        var quote = content.GetValue<string>("quote");
-            //        var authorUdi = content.GetValue<GuidUdi>("author");
-            //        var authorGuid = authorUdi.Guid;
-            //        var author = sender.GetById(authorGuid);
+            // Make form names reasonably unique (per user)
 
-            //        content.Name = Quotations.MakeNodeName(quote, author.GetValue<string>("firstName"), author.GetValue<string>("middleName"), author.GetValue<string>("lastName"));
+            var userId = _context.EnsureUmbracoContext().UmbracoContext.Security.CurrentUser.Id;
+            var suffix = "; #" + userId;
 
-            //        content.Properties["quoteSearchText"].SetValue(quote.StripHtml(false, false, true).RemovePunctuation(except: @"'\-"));
-            //        content.Properties["yearSearchText"].SetValue(content.GetValue<string>("year").StripHtml(false, false, true).RemovePunctuation(except: @"'\-"));
-
-            //        content.Properties["authorSearchText"].SetValue(Quotations.AuthorFullName(author.GetValue<string>("firstName"), author.GetValue<string>("middleName"), author.GetValue<string>("lastName")));
-
-            //        if (content.GetValue<GuidUdi>("source") != null)
-            //        {
-            //            var sourceUdi = content.GetValue<GuidUdi>("source");
-            //            var sourceGuid = sourceUdi.Guid;
-            //            var source = sender.GetById(sourceGuid);
-
-            //            content.Properties["sourceSearchText"].SetValue(source.GetValue<string>("sourceName").StripHtml(false, false, true).RemovePunctuation(except: @"'\-"));
-            //        }
-            //    }
-            //}
-
-            //foreach (var content in e.SavedEntities.Where(c => c.ContentType.Alias.InvariantEquals("person")))
-            //{
-            //    var _author = sender.GetById(content.Id);
-            //    var author = new Author(_context.EnsureUmbracoContext().UmbracoContext.ContentCache.GetById(content.Id));
-
-            //    content.Name = Strings.SortableName(author.FirstName, author.MiddleName, author.LastName);
-            //    content.Properties["nameSearchText"].SetValue(author.FullName.StripHtml(false, false, true));
-            //    content.Properties["biographySearchText"].SetValue(author.Biography.StripHtml(false, false, true));
-            //}
+            if (e.Form.Name.EndsWith(suffix) == false && userId > -1)
+            {
+                e.Form.Name = e.Form.Name + suffix;
+            }
         }
 
-        private void FormStorage_Created(IFormStorage sender, FormEventArgs e)
+        private void FormStorage_Saved(object sender, FormEventArgs e)
         {
+            // Iterate users, revoke form permission on users who are not admin or the form creator
 
+            var page = 0;
+            long total = 0;
+            var users = _userService.GetAll(page, 10, out total);
+            IEnumerable<Form> allForms = _formStorage.GetAllForms();
 
+            while (users.Any())
+            {
+                foreach (var user in users)
+                {
+                    var isAdmin = user.Groups.Where(g => g.Name == "Administrators").Count() > 0 || user.Id == -1;
+                    var suffix = "; #" + user.Id;
+
+                    foreach (Form form in allForms)
+                    {
+                        var formSecurityForUser = _userFormSecurityStorage.GetUserFormSecurity(user.Id.ToString(), form.Id);
+                        var hasSecurityAlready = (formSecurityForUser != null);
+
+                        if (!hasSecurityAlready)
+                        {
+                            formSecurityForUser = UserFormSecurity.Create();
+                            formSecurityForUser.User = user.Id.ToString();
+                            formSecurityForUser.Form = form.Id;
+                        }
+
+                        formSecurityForUser.HasAccess = false;
+
+                        if (form.Name.EndsWith(suffix) || isAdmin)
+                        {
+                            formSecurityForUser.HasAccess = true;
+                        }
+
+                        if (!hasSecurityAlready)
+                        {
+                            _userFormSecurityStorage.InsertUserFormSecurity(formSecurityForUser);
+                        }
+
+                        else
+                        {
+                            _userFormSecurityStorage.UpdateUserFormSecurity(formSecurityForUser);
+                        }
+                    }
+                }
+
+                page++;
+                users = _userService.GetAll(page, 10, out total);
+            }
         }
     }
 }
