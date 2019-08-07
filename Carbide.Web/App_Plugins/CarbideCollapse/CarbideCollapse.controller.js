42ce8f8de21d5f6fbd1be8f0cfe67eaa0c5bd0f5
Michael Argentini <michael@argentini.us>
2019-08-07 17:02:34 -0400

    CarbideCollapse now works with infinite editing

--- Carbide.Web/App_Plugins/CarbideCollapse/CarbideCollapse.controller.js
+++ Carbide.Web/App_Plugins/CarbideCollapse/CarbideCollapse.controller.js
@@ -9,21 +9,29 @@
                             || response.config.url.includes('umbraco/backoffice/UmbracoApi/Content/GetEmpty')) {
                         if (response.status === 200) {
                             window.setTimeout(function () {
-                                var tabs = document.getElementsByClassName("umb-group-panel__header");
-                                if (tabs) {
-                                    for (var i = 0; i < tabs.length; i++) {
-                                        tabs[i].classList.add("CarbideCollapse");
-                                        tabs[i].nextElementSibling.classList.add("CarbideCollapse");
-                                        if (tabs[i].onclick == null) {
-                                            tabs[i].onclick = function () {
-                                                this.classList.toggle("open");
-                                                this.nextElementSibling.classList.toggle("open");
+                                var containers = document.getElementsByClassName("umb-editor-container");
+                                if (containers) {
+                                    for (var c = 0; c < containers.length; c++) {
+                                        var tabs = containers[c].getElementsByClassName("umb-group-panel__header");
+                                        if (tabs) {
+                                            if (tabs.length > 0) {
+                                                for (var i = 0; i < tabs.length; i++) {
+                                                    tabs[i].classList.add("CarbideCollapse");
+                                                    tabs[i].nextElementSibling.classList.add("CarbideCollapse");
+                                                    if (tabs[i].onclick == null) {
+                                                        tabs[i].onclick = function () {
+                                                            this.classList.toggle("open");
+                                                            this.nextElementSibling.classList.toggle("open");
+                                                        }
+                                                    }
+                                                }
+                                                if (tabs[0]) {
+                                                    tabs[0].classList.add("open");
+                                                    tabs[0].nextElementSibling.classList.add("open");
+                                                }
                                             }
                                         }
-                                    }
-                                    if (tabs[0]) {
-                                        tabs[0].classList.add("open");
-                                        tabs[0].nextElementSibling.classList.add("open");
+
                                     }
                                 }
                             }, 500)
